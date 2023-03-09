dbc1dbcb3bb0a56bed184dab3b022bbdaf0730ac
Ben Brady <benbradybusiness@gmail.com>
2023-03-09 01:59:19 +0000

    Fixed rule34 indexing

--- src/js/booru/rule34.ts
+++ src/js/booru/rule34.ts
@@ -11,14 +11,16 @@ export const Rule34: Booru = {
 
     async search(query: Query, index: number = 0): Promise<Post[]> {
         let url = "https://api.rule34.xxx/index.php?page=dapi&s=post&q=index";
-        let search = query.search;
+        let params = new URLSearchParams({ "pid": index / 100, "limit": 100 });
 
+        let search = query.search;
         if (query.sort == Sort.HighestRated) { search += "sort:score" }
         else if (query.sort == Sort.LowestRated) { search += "sort:score:asc"}
         else if (query.sort == Sort.Hotest) { search += "sort:score"}
         else if (query.sort == Sort.Newest) { search += "sort:id"}
+        params.set("tags", search)
 
-        let r = await fetch(url + `&tags=${search}`);
+        let r = await fetch(url + "&" + params.toString());
         let body = await r.text();
         let posts = parse_xml(body, self.name);
         console.log(posts)
