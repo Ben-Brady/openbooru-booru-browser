971b2def97d31d0ed0023f7ee8cf05adebcb898e
Ben Brady <benbradybusiness@gmail.com>
2023-03-11 15:45:36 +0000

    Renamed Least Rated to Worst Rated

--- src/js/booru/query.ts
+++ src/js/booru/query.ts
@@ -7,7 +7,7 @@ export enum Sort {
 	Newest = "Newest First",
 	Hotest = "Hotest",
 	HighestRated = "Top Rated",
-	LowestRated = "Least Rated",
+	LowestRated = "Worst Rated",
 }
 
 export function encode_query(query: Query): string | undefined {
