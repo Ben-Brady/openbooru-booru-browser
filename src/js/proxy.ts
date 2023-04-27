6db7d2bcd40fde65f0d3a7965278d78c3ff8b82e
Ben Brady <benbradybusiness@gmail.com>
2023-04-27 23:57:56 +0100

    Updated CORS PROXY constant string

--- src/js/proxy.ts
+++ src/js/proxy.ts
@@ -1,14 +1,15 @@
+import { CORS_PROXY } from "js/config";
 export function generateUrl(url: URL | string | undefined): string | undefined {
 	if (url === undefined) {
 		return undefined
 	}
-	
+
 	url = new URL(url);
 	const isVideo = url.pathname.includes(".mp4") || url.pathname.includes(".webm");
 	const isBlocked = url.hostname.includes("rule34.xxx") && isVideo;
 
 	if (isBlocked) {
-		return "https://r34proxy.openbooru.workers.dev?url=" + encodeURIComponent(url.toString());
+		return CORS_PROXY + encodeURIComponent(url.toString());
 	} else {
 		return url.toString()
 	}
