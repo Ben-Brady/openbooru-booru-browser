975a9579dd624a0753d68d194270bba4d62139e3
Ben Brady <benbradybusiness@gmail.com>
2023-04-27 23:56:24 +0100

    Added CORS proxcy setting

--- src/js/config.ts
+++ src/js/config.ts
@@ -3,9 +3,10 @@ import {
 	PUBLIC_SITE_DESCRIPTION,
 	PUBLIC_SITE_URL,
 	PUBLIC_SITE_KEYWORDS,
+	PUBLIC_CORS_PROXY,
 } from "$env/static/public";
 export const SITE_NAME = PUBLIC_SITE_NAME;
 export const SITE_DESC = PUBLIC_SITE_DESCRIPTION;
 export const SITE_URL = PUBLIC_SITE_URL;
 export const SITE_KEYWORDS: string[] = PUBLIC_SITE_KEYWORDS.split(",");
-export const CORS_PROXY: string = "https://r34proxy.openbooru.workers.dev?url="
+export const CORS_PROXY = PUBLIC_CORS_PROXY;
