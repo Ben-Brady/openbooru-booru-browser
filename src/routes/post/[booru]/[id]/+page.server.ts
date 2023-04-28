e6ba1ffdb6f3532d1acc1dfb603c89dee2bbf37a
Ben Brady <benbradybusiness@gmail.com>
2023-04-28 01:51:50 +0100

    Attempts to load post on client

--- src/routes/post/[booru]/[id]/+page.server.ts
+++ src/routes/post/[booru]/[id]/+page.server.ts
@@ -1,23 +1,11 @@
 import type { PageServerLoad } from "./$types";
-import type { Post } from "js/booru/types";
+import { attempt_get_booru } from "js/booru";
 import { error } from "@sveltejs/kit";
-import { get_booru } from "js/booru";
-
-async function get_post(booru_name: string, id: string) {
-	const booru = get_booru(booru_name);
-	const post = await booru?.get(id);
-	if (post === undefined) throw 404;
-	return post;
-}
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { booru: booru_name, id } = params;
-
-	try {
-		let post: Post | undefined = await get_post(booru_name, id);
-		return { post, booru_name, id };
-	} catch (e: any) {
-		console.trace(e);
-		throw error(404, "Post Not Found");
-	}
+	const booru = attempt_get_booru(booru_name);
+	if (booru === undefined) throw error(404, "Booru Not Found");
+	const post = await booru?.get(id);
+	return { post, booru_name, id };
 };
--- src/routes/post/[booru]/[id]/+page.svelte
+++ src/routes/post/[booru]/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import { error } from "@sveltejs/kit";
 	import type { PageData } from "./$types";
-	import type { Post } from "js/booru/types";
 	import { get_booru } from "js/booru";
 	import { generate_post_link } from "js/links";
 	import HeadInfo from "lib/HeadInfo.svelte";
@@ -9,37 +9,35 @@
 	export let data: PageData;
 	const { post, booru_name, id } = data;
 
+	const booru = get_booru(booru_name);
+
 	async function getPost() {
-		const booru = get_booru(booru_name);
-		const post = await booru?.get(id);
+		const post = await booru.get(id);
+
 		if (post === undefined) {
 			location.href = "/";
-			throw 404;
 		} else {
 			return post;
 		}
 	}
 
-	function generateTitle(post: Post) {
-		const booru = get_booru(booru_name);
-		return `${booru?.display_name}: Post ${post.id}`;
-	}
+	const title = `${booru?.display_name}: Post ${id}`;
+	const link = generate_post_link(id, booru_name);
 </script>
 
 {#if post}
 	<HeadInfo
-		title="{generateTitle(post)}"
-		path="{generate_post_link(post.id, post.booru)}"
+		title="{title}"
+		path="{link}"
 		media="{post.full}"
 	/>
 	<PostPage post="{post}" />
 {:else}
+	<HeadInfo
+		title="Post"
+		path="{link}"
+	/>
 	{#await getPost() then post}
-		<HeadInfo
-			title="{generateTitle(post)}"
-			path="{generate_post_link(post.id, post.booru)}"
-			media="{post.full}"
-		/>
 		<PostPage post="{post}" />
 	{/await}
 {/if}
