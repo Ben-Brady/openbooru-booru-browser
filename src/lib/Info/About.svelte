c0f6845d8909c6563100719e8eb9b4c3ee105f4d
Ben Brady <benbradybusiness@gmail.com>
2023-04-28 12:41:16 +0100

    Updated Info

--- src/lib/Info/About.svelte
+++ src/lib/Info/About.svelte
@@ -2,8 +2,15 @@
 	export const csr = true;
 </script>
 
-Open Booru is a modern booru framework designed to replace existing implementations and allow
-for the creation of custom clients.
-<br /><br />
-Source Code:&nbsp;
-<a href="https://github.com/TheOpenBooru"> github.com/TheOpenBooru </a>
+<h1>About Us</h1>
+
+<div>
+	This site is based on a modified version of OpenBooru as a way to make it easier to access other booru's content.
+	It was inspired by r34.app, rule34.world as well as friendlyanon's userscripts.
+	With this site there were three key features I wanted to have:
+	<ul>
+		<li>Multi-booru Support</li>
+		<li>Improved Search</li>
+		<li>Column and Fullscreen Navigation</li>
+	</ul>
+</div>
--- src/lib/Info/Contact.svelte
+++ src/lib/Info/Contact.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
 	export const csr = true;
+	const domain = "openbooru.org";
+	const dmca_email = `dmca@${domain}`;
+	const support_email = `support@${domain}`;
+	const gdpr_email = `gdpr@${domain}`;
 </script>
 
-GDPR:&nbsp;
-<a href="{'mailto:gdpr@openbooru.org'}"> gdpr@openbooru.org </a>
-<br />
-Support:&nbsp;
-<a href="{'mailto:support@openbooru.org'}"> support@openbooru.org </a>
-<br />
-DMCA Contact:&nbsp;
-<a href="{'mailto:dmca@openbooru.org'}"> dmca@openbooru.org </a>
+<h1>Contact Us</h1>
+
+If you want to contact us, you can reach us through the following emails:
+
+<div>
+	<br />
+	<a href={`mailto:dmac@${domain}`}>
+		dmca@openbooru.org
+	</a>
+</div>
+If you want to have your content removed from the site.<br/>
+Note: Although we comply with requests and blacklist content, we do not host the content so you'll have file another request with the post's source as well
+
+<div>
+	<br/>
+	<a href={`mailto:gdpr@${domain}`}>
+		gdpr@openbooru.org
+	</a>
+</div>
+
+If you need support on any issues involving GDPR such as data deletion or requesting your data.
+
+<div>
+	<br />
+	<a href={`mailto:support@${domain}`}>
+		support@openbooru.org
+	</a>
+</div>
+If you need any other help more general help involving the site. This can be anything from suggestions to inqueries, we'll be happy to respond.
