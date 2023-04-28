d1da3fa6a5ca2b7e89ce43dfd6410b22a09b704c
Ben Brady <benbradybusiness@gmail.com>
2023-04-28 13:37:50 +0100

    Added cache to the posts page

--- src/routes/post/[booru]/[id]/+page.server.ts
+++ src/routes/post/[booru]/[id]/+page.server.ts
@@ -1,5 +1,6 @@
 import type { PageServerLoad } from "./$types";
 import { attempt_get_booru } from "js/booru";
+import Cache from "js/cache";
 import { error } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ params }) => {
@@ -7,7 +8,8 @@ export const load: PageServerLoad = async ({ params }) => {
 	const booru = attempt_get_booru(booru_name);
 	if (booru === undefined) throw error(404, "Booru Not Found");
 
-	let post = await (async () => {
+	let key = `post-${id}-${booru_name}`
+	let post = await Cache.use_cache_async(key, 5, async () => {
 		try {
 			const post = await booru.get(id);
 			if (post === undefined) throw error(404, "Post Not Found");
@@ -15,6 +17,6 @@ export const load: PageServerLoad = async ({ params }) => {
 		} catch (e) {
 			return undefined;
 		}
-	})()
+	});
 	return { post, booru_name, id };
 };
