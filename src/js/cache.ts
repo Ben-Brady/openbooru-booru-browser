1b10b4679789dcb0e420267ed0fc6aed16b25926
Ben Brady <benbradybusiness@gmail.com>
2023-04-28 13:37:40 +0100

    Cache is now supported on the server

--- src/js/cache.ts
+++ src/js/cache.ts
@@ -9,40 +9,52 @@ function generate_cache_key(key: string) {
 	return "cache-" + key;
 }
 
+
+let _store = new Map<string, string>();
+let serverStorage = {
+	getItem: (key: string): string|null => {
+		let value = _store.get(key);
+		if (value === undefined) return null;
+		else return value;
+	},
+	setItem: _store.set,
+	removeItem: _store.delete,
+	clear: _store.clear
+}
+
 function get(key: string): null | any {
+	let storage = browser ? sessionStorage : serverStorage;
 	let cache_key = generate_cache_key(key);
-	let json = sessionStorage.getItem(cache_key);
+
+	let json = storage.getItem(cache_key);
 	if (json == null) return null;
 
 	let store: CacheStore = JSON.parse(json);
 	if (Date.now() > store.expiration) {
-		sessionStorage.removeItem(cache_key);
+		storage.removeItem(cache_key);
 		return null;
 	}
 
 	return store.value;
 }
 
-function set(key: string, value: any, ttl: number = 5) {
+function set(key: string, value: any, ttl: number = 5000) {
+	let storage = browser ? sessionStorage : serverStorage;
 	let cache_key = generate_cache_key(key);
 
 	let store = {
 		value: value,
-		expiration: Date.now() + ttl * 1000,
+		expiration: Date.now() + ttl
 	};
 
 	try {
-		sessionStorage.setItem(cache_key, JSON.stringify(store));
+		storage.setItem(cache_key, JSON.stringify(store));
 	} catch {
-		sessionStorage.clear()
+		storage.clear()
 	}
 }
 
-function use_cache<T>(key: string, ttl: number = 5, func: () => T): T {
-	if (!browser) {
-		return func();
-	}
-
+function use_cache<T>(key: string, ttl: number = 5000, func: () => T): T {
 	let cached_value = get(key);
 	if (cached_value) {
 		return cached_value;
@@ -53,11 +65,7 @@ function use_cache<T>(key: string, ttl: number = 5, func: () => T): T {
 	}
 }
 
-async function use_cache_async<T>(key: string, ttl: number = 5, func: () => Promise<T>): Promise<Awaited<T>> {
-	if (!browser) {
-		return await func();
-	}
-
+async function use_cache_async<T>(key: string, ttl: number = 5000, func: () => Promise<T>): Promise<Awaited<T>> {
 	let cached_value = get(key);
 	if (cached_value) {
 		return cached_value;
